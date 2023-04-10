1c3bdc1661185a2c93639bf1bc23192de2060e74
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-04-10 15:10:09 +0530

    created api endpoint for searching the user and created a middleware for user authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { registerUser, authUser } = require('../controlers/userController')
+const { registerUser, authUser, allUsers } = require('../controlers/userController');
+const { protect } = require('../middleware/authMiddleware');
 const router = express.Router()
 
-router.route('/').post(registerUser)
-router.post('/login',authUser)
+router.route('/').post(registerUser).get(protect,allUsers);
+router.post('/login',authUser);
 
-module.exports = router;
+module.exports = router; 
