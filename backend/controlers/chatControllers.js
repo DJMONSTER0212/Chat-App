ad1f13fcdb4d580ee3617f18c2e99433b8450d95
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-04-18 00:27:08 +0530

    created api endpoints for adding and removing a user from groupchat

--- backend/controlers/chatControllers.js
+++ backend/controlers/chatControllers.js
@@ -109,6 +109,44 @@ const renameGroup = asyncHandler(async(req,res)=>{
         }else{
             res.json(updatedChat);
         }
+});
+
+const addToGroup = asyncHandler(async(req,res)=>{
+    const {chatId,userId} = req.body;
+
+    const added = await Chat.findByIdAndUpdate(
+        chatId,
+        {$push:{users:userId}},
+        {new:true}
+    )
+    .populate("users","-password")
+    .populate("groupAdmin","-password");
+    if(!added){
+        res.status(404);
+        throw new Error("Chat not found");
+    }
+    else {
+        res.json(added);
+    }
 })
 
-module.exports = { accessChat, fetchChats, createGroupChat, renameGroup }
+const removeFromGroup = asyncHandler(async (req, res) => {
+    const { chatId, userId } = req.body;
+
+    const removed = await Chat.findByIdAndUpdate(
+        chatId,
+        { $pull: { users: userId } },
+        { new: true }
+    )
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password");
+    if (!removed) {
+        res.status(404);
+        throw new Error("Chat not found");
+    }
+    else {
+        res.json(removed);
+    }
+})
+
+module.exports = { accessChat, fetchChats, createGroupChat, renameGroup, addToGroup, removeFromGroup }
